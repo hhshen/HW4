07fba339bc913f5b8e199293fa9330dfa4003906
hhshen <hhshen@umich.edu>
2016-02-04 00:19:42 -0500

    Update reverse_sort.c

--- reverse_sort.c
+++ reverse_sort.c
@@ -1,18 +1,9 @@
-#include <string.h>
-#include <stdint.h>
-#include <stdlib.h>
-#include <stdbool.h>
 
-void reverse(int *numbers, unsigned count) {
-	int temp;
-	int i;
-	int j = count;
-	unsigned half = count/2;
+int compare(int a, int b) {
+	    return b - a;
+}
 
-	for (i = 0; i < half; i++) {
-		temp = numbers[i];
-		numbers[i] = numbers[j];
-		numbers[j] = temp;
-		j--;
-	}
+int wrap(const void *a, const void *b) {
+	
+	return ( *(int *)b - *(int *)a );
 }
